33e9dd6e4c1e63f9f973d8c20c4649e0ed8cf441
yusri009 <197112727+yusri009@users.noreply.github.com>
2025-09-16 12:39:16 +0530

    deleted unnecessary codes

--- Frontend/src/app/page.js
+++ Frontend/src/app/page.js
@@ -5,19 +5,8 @@ import Header from "./components/Header/header.jsx";
 import CounterSection from "./components/Counter/counterSection.jsx";
 import Categories from "./components/Categories/Categories.jsx";
 import ReachUs from "./components/ReachUs/ReachUs.jsx";
-// import Login from "./components/LogIn/login.jsx";
 
 export default function Home() {
-  // useEffect(() => {
-  //   // Clear localStorage on every page load/refresh
-  //   localStorage.clear();
-    
-  //   // Or if you want to clear only specific items:
-  //   // localStorage.removeItem('cart');
-    
-  //   console.log('📭 localStorage cleared on page refresh');
-  // }, []);
-
   return (
     <>
       <Header />
